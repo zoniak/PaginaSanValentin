e322eff92ea9f5873b21b4c1143f32c38d1d67d9
osopardo895 <alexcesarfer1234@gmail.com>
2024-02-07 13:18:14 +0100

    PaginaSanValentin

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.getElementById("siBtn").addEventListener("click", function () {
 
   // Mostrar el mensaje específico
   document.getElementById("messageContainer").style.display = "block";
-  document.getElementById("messageContainer").innerHTML = "¡aaaaAAAaaaa snwajdn!";
+  document.getElementById("messageContainer").innerHTML = "Buenos dias mi amol UwU";
 
   // Mostrar otro gif después de 3 segundos
   setTimeout(function () {
